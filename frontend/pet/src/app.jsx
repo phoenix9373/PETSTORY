34be2b8ddaa4d082b2466d31be6c810791055ed9
전의수 <jes5918@koreatech.ac.kr>
2021-01-28 00:02:42 +0900

    S04P12B204-140 [fix] : FE - CORS에러 Proxy로 해결

--- frontend/pet/src/app.jsx
+++ frontend/pet/src/app.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './app.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import landingPage from './components/LandingPage/landingPage';
-import loginPage from './components/LoginPage/loginPage';
-import registerPage from './components/RegisterPage/registerPage';
-import Auth from './hoc/auth';
+import LandingPage from './views/LandingPage/LandingPage';
+import LoginPage from './views/LoginPage/LoginPage';
+import RegisterPage from './views/RegisterPage/RegisterPage';
+import NavBar from './views/NavBar/NavBar';
+// import Auth from './hoc/auth';
 
 function App() {
   return (
     <Router>
       <div>
+        <NavBar />
         <Switch>
-          <Route exact path="/" component={Auth(landingPage, null)} />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          {/* <Route exact path="/" component={Auth(landingPage, null)} />
           <Route exact path="/login" component={Auth(loginPage, false)} />
-          <Route exact path="/register" component={Auth(registerPage, false)} />
+          <Route exact path="/register" component={Auth(registerPage, false)} /> */}
         </Switch>
       </div>
     </Router>
--- frontend/pet/src/hoc/auth.js
+++ frontend/pet/src/hoc/auth.js
@@ -13,24 +13,23 @@ export default function (Componet, option, adminRoute = null) {
     // useEffect를 사용해서 초기 검증을 실행해준다
     useEffect(() => {
       dispatch(authUser()).then((res) => {
-        console.log(res);
-        // console.log("훅 테스트");
-        // 로그인을 하지 않았을때
+        console.log('훅 테스트', res);
+        // // 로그인을 하지 않았을때
         // if (!res.payload.isAuth) {
         //   if (option) {
         //     // option이 true일때 로그인으로 강제 이동
         //     props.history.push('/login');
         //   }
-        // }
-
-        // else {
+        // } else {
         //   // 로그인을 했을때
         //   // 관리자인 경우
         //   if (adminRoute && res.payload.isAdmin) {
         //     // 관리자 페이지로 이동
+        //     props.history.push('/login');
         //   } else {
         //     // 관리자가 아닌 경우
         //     // option이 false일때 LandingPage로 이동
+        //     console.log('test');
         //     if (!option) {
         //       props.history.push('/');
         //     }
