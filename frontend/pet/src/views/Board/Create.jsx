7ca840ab54e470df64f5aae4dbd26fc71d09cc91
신민호 <eeqwer123@gmail.com>
2021-02-03 00:08:11 +0900

    S04P13B204-18 [feat] : FE - 백과 통신연결 구현

--- frontend/pet/src/views/Board/Create.jsx
+++ frontend/pet/src/views/Board/Create.jsx
@@ -8,30 +8,32 @@ import axios from 'axios';
 
 export default function Create() {
   const titleRef = React.createRef();
-  const contentRef = React.createRef();
+  const contextRef = React.createRef();
   const [images, setImages] = React.useState([]);
-  // const [title, setTitle] = React.useState('');
-  // const [content, setContent] = React.useState('');
   const maxNumber = 69;
-  const onChange = (imageList, addUpdateIndex) => {
+  const files = new FormData();
+  const onlistChange = (imageList, addUpdateIndex) => {
     // data for submit
-    console.log(imageList, addUpdateIndex);
-    setImages(imageList);
+    for (const image of imageList) {
+      files.append('files', image.file);
+    }
     // axios 통신하는 위치
   };
+
   const onSubmit = (event) => {
     event.preventDefault();
-    const obj = {
+    const request = {
       title: titleRef.current.value,
-      content: contentRef.current.value,
-      imageList: images,
-
+      context: contextRef.current.value,
     };
-    console.log(obj);
-    axios.post('http://localhost:8080/board/create', obj)
-        .then(res => console.log(res.data));
+    files.append('title', request.title);
+    files.append('context', request.context);
+    axios.post("/api/board/createV2", files, {})
+      .then(response => console.log(response))
+      .then(result => console.log(result))
+      .catch(error => console.error('error', error));
     titleRef.current.value = '';
-    contentRef.current.value = '';
+    contextRef.current.value = '';
     setImages('');
   };
 
@@ -40,7 +42,7 @@ export default function Create() {
       <ImageUploading
         multiple
         value={images}
-        onChange={onChange}
+        onChange={onlistChange}
         maxNumber={maxNumber}
         dataURLKey="data_url"
       >
@@ -54,21 +56,21 @@ export default function Create() {
           dragProps,
         }) => (
           // write your building UI
-          <div className={isDragging ? "imageupload active" : "imageupload"}
+          <div
           {...dragProps}>
-            <button
+            <div
               className="uploadimg"
               onClick={onImageUpload}
             >
               <img src={upload} alt="왜"/>
 
-            </button>
-            <button onClick={onImageRemoveAll}>RESET</button>
+            </div>
+            <div onClick={onImageRemoveAll}>RESET</div>
             &nbsp;
             <Grid container spacing={1}>
-            {imageList.map((image, index) => (
+            {imageList.map((img, index) => (
               <Grid key={index} className="image-item">
-                <img src={image.data_url} alt="" width="100" />
+               <img src={img.data_url} alt="어디써" width="100" />
                 <div className="image-item__btn-wrapper">
                   <button onClick={() => onImageUpdate(index)}><i className="fas fa-pen color-yellow"></i></button>
                   <button onClick={() => onImageRemove(index)}><i className="fas fa-times-circle color-red"></i></button>
@@ -84,13 +86,10 @@ export default function Create() {
       </ImageUploading>
       <div className="text-div">
           <input ref={titleRef} type="text" placeholder="제목을 입력하세요"/>
-          <input ref={contentRef} type="text" placeholder="내용을 입력하세요"/>
+          <input ref={contextRef} type="text" placeholder="내용을 입력하세요"/>
 
       </div>
       <button>생성하기</button>
     </form>
   );
 }
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<Create />, rootElement);
