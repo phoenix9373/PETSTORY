3f6653d2d29946ff3ab5ff423166147f75914724
유진우 <phoenix9373@naver.com>
2021-02-08 07:09:40 +0000

    S04P12B204-58 [feat] : FE - 피드 MUI 적용

--- frontend/pet/src/views/MainPage/MainPage.jsx
+++ frontend/pet/src/views/MainPage/MainPage.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import FeedDetail from '../../components/Feed/FeedDetail';
-import FeedFrame from '../../components/Feed/FeedFrame';
 import FeedInfinite from '../../components/Feed/FeedInfinite';
-import FeedInfiniteCopy from '../../components/Feed/FeedInfiniteCopy';
+import FeedTest from '../../components/Feed/FeedTest';
 
 function MainPage(props) {
   return (
     <div>
-      {/* <FeedFrame /> */}
       {/* <FeedDetail /> */}
       <FeedInfinite />
+      {/* <FeedTest /> */}
     </div>
   );
 }
