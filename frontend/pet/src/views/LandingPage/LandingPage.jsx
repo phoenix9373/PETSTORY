dd214d702b7c32a57b4fd8fea79708c2e86ac225
전의수 <jes5918@koreatech.ac.kr>
2021-02-09 17:51:47 +0900

    S04P13B204-95 [feat] : FE - 첫 페이지 버블 작성완료

--- frontend/pet/src/views/LandingPage/LandingPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-// import NavBar from '../../components/NavBar/NavBar';
-
-function LandingPage() {
-  return <>{/* <NavBar /> */}</>;
-}
-export default withRouter(LandingPage);
