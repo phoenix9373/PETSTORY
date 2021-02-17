528d741561dc57b793ff6cebabfa9c40a46cf919
multicampus <shzmfkdl@naver.com>
2021-02-17 22:52:16 +0900

    S04P13B204-142 [feat] : FE - 알람수 요청 후 nav로 넘기기

--- frontend/src/components/ProfileModal/OneProfile.jsx
+++ frontend/src/components/ProfileModal/OneProfile.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { deleteProfile } from '../../_actions/profileAction';
+import { useSelector, useDispatch } from 'react-redux';
+import { saveAlarmNum } from '../../_reducers/alarmReducer';
+import { deleteProfile, getAlarmNumdddd } from '../../_actions/profileAction';
+
 // component, css
 import ModifyProfile from './ModifyProfile';
-import style from './OneProfile.module.css';
+import styles from './OneProfile.module.css';
 // library
 import toast from 'react-hot-toast';
 // material UI
@@ -18,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
+      display: 'none',
     },
     '&:hover > .editIcon': {
       display: 'inline-block',
@@ -34,12 +37,21 @@ function OneProfile(props) {
   const [isModal, setIsModal] = useState(false);
   const [check, setCheck] = useState(false);
 
+  // 프로필ID에 해당하는 알람수 요청 -> redux에 값 저장
   const dispatch = useDispatch();
+  const getAlarm = async (profileId) => {
+    await dispatch(getAlarmNumdddd(profileId)).then((res) => {
+      dispatch(saveAlarmNum(res.payload)); // store의 state 바꾸는 함수 실행
+    });
+  };
 
   // local에 profileId 저장
   const saveProfileId = (profileId) => {
     localStorage.removeItem('profileId'); // 기존 id 삭제
     localStorage.setItem('profileId', profileId); // 새로 저장
+
+    getAlarm(profileId);
+    // props.handleChangeProfileId(profileId); // app에 profileId 변한 걸 알려주기
   };
 
   // 수정
@@ -84,48 +96,39 @@ function OneProfile(props) {
   };
 
   return (
-    <div>
-      <li>
-        {/* 프로필 이미지 */}
-        <div className={style.oneOfProfiles}>
-          <Link
-            to={'/'}
-            onClick={() => {
-              saveProfileId(props.item.profileId);
-            }}
-          >
-            <img
-              className={style.oneOfProfileImg}
-              src={props.item.imgFullPath}
-              alt="프로필이미지"
-            />
-            <h2>{props.item.nickname}</h2>
-            {/* 수정, 삭제 아이콘 */}
-          </Link>
-          <div className={classes.root}>
-            <Fab className={classes.editIcon} color="secondary">
-              <EditIcon
-                className={classes.editIcon}
-                onClick={handleModifyModal}
-              />
-            </Fab>
-            <Fab className={classes.editIcon} color="primary">
-              <HighlightOffIcon
-                className={style.deleteIcon}
-                onClick={handleDeleteProfile}
-              />
-            </Fab>
-            {/* 수정 모달 */}
-            <ModifyProfile
-              isOpen={isModal}
-              profile={props.item}
-              closeModal={closeModifyModal}
-              handleModify={handleModify}
-            />
-          </div>
-        </div>
-      </li>
-    </div>
+    <li className={styles.li}>
+      {/* 프로필 이미지 */}
+      <Link
+        className={styles.link}
+        to={'/'}
+        onClick={() => {
+          saveProfileId(props.item.profileId);
+        }}
+      >
+        <img
+          className={styles.img}
+          src={props.item.imgFullPath}
+          alt="프로필이미지"
+        />
+        <span className={styles.nickname}>{props.item.nickname}</span>
+        {/* 수정, 삭제 아이콘 */}
+      </Link>
+      <div className={styles.icons}>
+        <Fab className={styles.icon} color="primary">
+          <EditIcon onClick={handleModifyModal} />
+        </Fab>
+        <Fab className={styles.icon} color="secondary">
+          <HighlightOffIcon onClick={handleDeleteProfile} />
+        </Fab>
+        {/* 수정 모달 */}
+        <ModifyProfile
+          isOpen={isModal}
+          profile={props.item}
+          closeModal={closeModifyModal}
+          handleModify={handleModify}
+        />
+      </div>
+    </li>
   );
 }
 export default OneProfile;
